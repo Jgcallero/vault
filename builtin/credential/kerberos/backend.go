6bca812ed45b4232d17f9ab7fbc32e47a3e4792c
jgcallero <jg_callero@hotmail.com>
2016-08-26 11:54:13 -0700

    Added more skeleton code

--- builtin/credential/kerberos/backend.go
+++ builtin/credential/kerberos/backend.go
@@ -1,1 +1,25 @@
 package kerberos
+
+import (
+	"github.com/hashicorp/vault/logical"
+	"github.com/hashicorp/vault/logical/framework"
+)
+
+func Factory(conf *logical.BackendConfig) (logical.Backend, error) {
+	return Backend().Setup(conf)
+}
+
+func Backend() *backend {
+
+	var b backend
+
+	return &b
+}
+
+type backend struct {
+	*framework.Backend
+}
+
+const backendHelp = `
+Text
+`
--- builtin/credential/kerberos/path_login.go
+++ builtin/credential/kerberos/path_login.go
@@ -1,1 +1,32 @@
 package kerberos
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/vault/helper/policyutil"
+	"github.com/hashicorp/vault/logical"
+	"github.com/hashicorp/vault/logical/framework"
+)
+
+func pathLogin(b *backend) *framework.Path {
+
+}
+
+func (b *backend) pathLogin(
+	req *logical.Request, d *framework.FieldData) (*logical.Response, error) {
+
+}
+
+func (b *backend) pathLoginRenew(
+	req *logical.Request, d *framework.FieldData) (*logical.Response, error) {
+
+}
+
+const pathLoginSyn = `
+Placeholder
+`
+
+const pathLoginDesc = `
+Placeholder
+`
